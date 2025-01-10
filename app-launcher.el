31289d1150d8e26190d0600a68cd21ee025e5418
Sergey Trofimov <sarg@sarg.org.ru>
2025-01-10 09:09:18 +0100

    Simplify desktop file parsing.

--- app-launcher.el
+++ app-launcher.el
@@ -76,67 +76,31 @@ This function always returns its elements in a stable order."
 		(puthash id file hash)))))))
     result))
 
+(defun app-launcher--is-installed (tryexec)
+  "Check if TRYEXEC file is installed in the \"exec-path\"."
+  (or (not tryexec)
+      (locate-file tryexec exec-path nil #'file-executable-p)))
+
 (defun app-launcher-parse-files (files)
-  "Parse the .desktop files to return usable informations."
+  "Parse the .desktop FILES to return usable informations."
   (let ((hash (make-hash-table :test #'equal)))
     (dolist (entry files hash)
-      (let ((file (cdr entry)))
-	(with-temp-buffer
-	  (insert-file-contents file)
-	  (goto-char (point-min))
-	  (let ((start (re-search-forward "^\\[Desktop Entry\\] *$" nil t))
-		(end (re-search-forward "^\\[" nil t))
-		(visible t)
-		name comment exec path)
-	    (catch 'break
-	      (unless start
-		(message "Warning: File %s has no [Desktop Entry] group" file)
-		(throw 'break nil))
+      (when-let* ((file (cdr entry))
+                  (parsed (xdg-desktop-read-file file))
+                  (name (gethash "Name" parsed))
+                  (exec (gethash "Exec" parsed))
+                  ((string= (gethash "Type" parsed) "Application"))
+                  ((app-launcher--is-installed (gethash "TryExec" parsed))))
 
-	      (goto-char start)
-	      (when (re-search-forward "^\\(Hidden\\|NoDisplay\\) *= *\\(1\\|true\\) *$" end t)
-		(setq visible nil))
-	      (setq name (match-string 1))
-
-	      (goto-char start)
-	      (unless (re-search-forward "^Type *= *Application *$" end t)
-		(throw 'break nil))
-	      (setq name (match-string 1))
-
-	      (goto-char start)
-	      (unless (re-search-forward "^Name *= *\\(.+\\)$" end t)
-		(push file counsel-linux-apps-faulty)
-		(message "Warning: File %s has no Name" file)
-		(throw 'break nil))
-	      (setq name (match-string 1))
-
-	      (goto-char start)
-	      (when (re-search-forward "^Comment *= *\\(.+\\)$" end t)
-		(setq comment (match-string 1)))
-
-	      (goto-char start)
-	      (when (re-search-forward "^Path *= *\\(.+\\)$" end t)
-		(setq path (match-string 1)))
-
-	      (goto-char start)
-	      (unless (re-search-forward "^Exec *= *\\(.+\\)$" end t)
-		;; Don't warn because this can technically be a valid desktop file.
-		(throw 'break nil))
-	      (setq exec (match-string 1))
-
-	      (goto-char start)
-	      (when (re-search-forward "^TryExec *= *\\(.+\\)$" end t)
-		(let ((try-exec (match-string 1)))
-		  (unless (locate-file try-exec exec-path nil #'file-executable-p)
-		    (throw 'break nil))))
-
-	      (puthash name
-		       (list (cons 'file file)
-			     (cons 'exec exec)
-                             (cons 'path path)
-			     (cons 'comment comment)
-			     (cons 'visible visible))
-		       hash))))))))
+        (puthash name
+                 (list (cons 'file file)
+                       (cons 'exec exec)
+                       (cons 'path (gethash "Path" parsed))
+                       (cons 'comment (gethash "Comment" parsed))
+                       (cons 'visible
+                             (not (string= "true" (or (gethash "Hidden" parsed)
+                                                      (gethash "NoDisplay" parsed))))))
+                 hash)))))
 
 (defun app-launcher-list-apps ()
   "Return list of all Linux .desktop applications."
@@ -199,3 +163,5 @@ When ARG is non-nil, ignore NoDisplay property in *.desktop files."
 
 ;; Provide the app-launcher feature
 (provide 'app-launcher)
+
+;;; app-launcher.el ends here
